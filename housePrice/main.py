f111b4833082635c93cc86c06a1f62f2bc0fd725
itchencheng <itchencheng@gmail.com>
2018-08-23 23:54:26 +0800

    redo housePrice contest, learn pandas, try xgboost again

---
+++ housePrice/main.py
@@ -0,0 +1,36 @@
+#coding:utf-8
+'''
+*******************************************************************************
+*       Filename:  main.py
+*    Description:  py file
+*       
+*        Version:  1.0
+*        Created:  2018-08-11
+*         Author:  chencheng
+*
+*        History:  initial draft
+* 
+* https://blog.csdn.net/c2a2o2/article/details/77646025
+* https://zhuanlan.zhihu.com/p/28739256
+* kaggle: https://www.kaggle.com/c/titanic
+*******************************************************************************
+'''
+
+import numpy as np
+import pandas as pd
+import xgboost
+import sklearn.model_selection
+
+
+def xgboost_method():
+	print("# xgboosted!")
+
+
+def main():
+	xgboost_method()
+
+	print("#end!")
+
+
+if __name__ == "__main__":
+	main()
--- titanic/xgboost.py
+++ titanic/xgboost.py
@@ -42,9 +42,36 @@ def CheckNaN(df):
 
 
 def CleanData(titanic):
-    titanic["Age"] = titanic["Age"].fillna(titanic["Age"].median())
+    # name
+    def getName(name):
+        if "Mrs" in str(name):
+            return 1
+        elif "Mr" in str(name):
+            return 2
+        elif "Master" in str(name):
+            return 3
+        elif "Miss" in str(name):
+            return 4
+        else:
+            return 0
 
-    titanic["child"] = titanic["Age"].apply(lambda x: 1 if x < 15 else 0)
+    titanic["name"] = titanic["Name"].apply(getName)
+
+    titanic["Age"] = titanic["Age"].fillna(titanic["Age"].median())
+    #titanic["Age"] = titanic["Age"].fillna(-1)
+
+    def childLevel(age_in):
+        if age_in > 50:
+            age = 1
+        elif age_in > 14:
+            age = 2
+        elif age_in > 5:
+            age = 3
+        else:
+            age = 0
+        return age
+    #titanic["child"] = titanic["Age"].apply(lambda x: 1 if x < 15 else 0)
+    titanic["child"] = titanic["Age"].apply(childLevel)
 
     titanic["sex"] = titanic["Sex"].apply(lambda x: 1 if x == "male" else 0)
 
@@ -59,7 +86,7 @@ def CleanData(titanic):
             return 3
     titanic["embark"] = titanic["Embarked"].apply(getEmbark)
 
-    titanic["fimalysize"] = titanic["SibSp"] + titanic["Parch"] + 1
+    titanic["familysize"] = titanic["SibSp"] + titanic["Parch"] + 1
 
     # cabin
     def getCabin(cabin):
@@ -69,15 +96,6 @@ def CleanData(titanic):
             return 1
     titanic["cabin"] = titanic["Cabin"].apply(getCabin)
 
-    # name
-    def getName(name):
-        if "Mrs" in str(name):
-            return 1
-        elif "Mr" in str(name):
-            return 2
-        else:
-            return 0
-    titanic["name"] = titanic["Name"].apply(getName)
 
     titanic["Fare"] = titanic["Fare"].fillna(titanic["Fare"].median())
 
@@ -105,18 +123,40 @@ def main():
 
 
     ''' feature engineering '''
-    features = ["Pclass", "sex", "child", "fimalysize", "Fare", "embark", "cabin"]
+    #features = ["Pclass", "sex", "child", "familysize", "Fare", "embark"]#, "cabin", "name"]
+    features = ["Pclass", "sex", "name", "child", "familysize", "Fare", "embark"]
 
     ''' build model '''
     #clf = xgboost.XGBClassifier(learning_rate=0.1, max_depth=5, n_estimators=100,
     #                            silent=False, objective='binary:logistic')
+    #clf = xgboost.XGBClassifier(learning_rate=0.1, max_depth=6, n_estimators=32,
+    #                            silent=True, objective='binary:logistic')
+    
+    #clf.fit(train_data[features], train_data["Survived"])
+
+    ''' grid search '''
     clf = xgboost.XGBClassifier(learning_rate=0.1, max_depth=6, n_estimators=32,
                                 silent=True, objective='binary:logistic')
+    param_test = {
+        'n_estimators': range(30, 100, 2),
+        'max_depth': range(2, 5, 1)
+    }
+    grid_search = sklearn.model_selection.GridSearchCV(estimator = clf, param_grid = param_test, 
+                                scoring='accuracy', cv = 8) #should learn more about the kFold(cv value)
+    grid_search.fit(train_data[features], train_data["Survived"])
 
-    clf.fit(train_data[features], train_data["Survived"])
+    best_clf = grid_search.best_estimator_
+ 
 
-    predictions = clf.predict(test_data[features])
+    print("# grid_scores_:")
+    for item in grid_search.grid_scores_:
+        print(item)
+    print("# best_params_:")
+    print(grid_search.best_params_)
+    print("#best_score_")
+    print(grid_search.best_score_)
 
+    predictions = best_clf.predict(test_data[features])
     
     submission = pd.DataFrame({'PassengerId': test_data['PassengerId'],
                                 'Survived': predictions})
